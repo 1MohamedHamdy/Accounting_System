03fba4a23c2ded26cf9a1fe1f0ba4cc299a9331d
1MohamedHamdy <mohamedhamdyabdelsamie@gmail.com>
2025-06-04 12:52:44 +0300

    Test2

--- project/settings.py
+++ project/settings.py
@@ -1,18 +1,25 @@
 import os
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = 'django-insecure-j-h3pvnm5j&up_tx5912#)k%y!e86=mo7=ml%k6((7lmx=s-j2'
-DEBUG = True
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+#SECRET_KEY = 'django-insecure-j-h3pvnm5j&up_tx5912#)k%y!e86=mo7=ml%k6((7lmx=s-j2'
+#DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000',
     'https://localhost:8000',
     'https://capable-leopard-properly.ngrok-free.app',
-    'http://capable-leopard-properly.ngrok-free.app,'
-    
+    'http://capable-leopard-properly.ngrok-free.app,',
+    'https://accountingsystem-production-06ef.up.railway.app',
+    'http://accountingsystem-production-06ef.up.railway.app',
 ]
 
 INSTALLED_APPS = [
@@ -63,14 +70,28 @@ TEMPLATES = [
 # Add to the bottom
 ADMIN_SITE_HEADER = "Accounting System Administration"
 
-WSGI_APPLICATION = 'project.wsgi.application'
+ASGI_APPLICATION = "project.asgi.application"
+WSGI_APPLICATION = "project.wsgi.application"
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    # DATABASES = {
+    #     "default": {
+    #         "ENGINE": "django.db.backends.sqlite3",
+    #         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    #     }
+    # }
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "railway",  # Replace with your actual DB name
+            "USER": "postgres",  # Replace with your actual DB user
+            "PASSWORD": "yGIwdmOJPOKMYkVXByIeegbpnROKKDaW",  # Replace with your actual DB password
+            "HOST": "postgres.railway.internal",  # Use Railway's host
+            "PORT": "5432",  # PostgreSQL default port
+        }
     }
-}
+    
+    
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -109,15 +130,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
-    }
-}
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django_redis.cache.RedisCache',
+#         'LOCATION': 'redis://127.0.0.1:6379/1',
+#         'OPTIONS': {
+#             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+#         }
+#     }
+# }
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
